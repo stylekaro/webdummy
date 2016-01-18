eb97d07b8a6693e17ceacb47f3d48afbf794578a
eraktri <eraktri@DJJ48R1.ericsson.se>
2016-01-18 16:57:35 +0530

    Creation of Basic User Page
    1. Creation of user.html with user.css and user.js
    2. Modifications on salon page accordingly.

---
+++ public_html/js/user.js
@@ -0,0 +1,188 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var strVar = "";
+strVar += " <div class=\"reviewItem\">";
+strVar += "                                    <div class=\" row reviewItemHeader\">";
+strVar += "                                        <div class=\"col-lg-1 col-md-1 col-sm-1 col-xs-1 zeroPadding\">";
+strVar += "                                            <a href=\"#\"><img src=\"img\/savatar1.png\" class=\"img-responsive\" style=\"width: 34px;height: 34px;\"><\/a>";
+strVar += "                                        <\/div>";
+strVar += "                                        <div class=\"col-lg-10 col-md-10 col-sm-10 col-xs-10\">";
+strVar += "                                            <div class=\"row\">";
+strVar += "                                                <a class=\"reviewItemHeaderName\" href=\"salon.html\">Salon 1<\/a>";
+strVar += "                                            <\/div>";
+strVar += "                                            <div class=\"row\">";
+strVar += "                                                <a class=\"reviewItemHeaderDetails\"> Elgin, Kolkata<\/a>";
+strVar += "                                            <\/div>";
+strVar += "                                        <\/div>";
+strVar += "                                        <div class=\"col-lg-1 col-md-1 col-sm-1 col-xs-1 zeroPadding\">";
+strVar += "                                            <span class=\"btn btn-default\"><i class=\"fa fa-heart-o\"><\/i><\/span>";
+strVar += "                                        <\/div>";
+strVar += "                                    <\/div>";
+strVar += "                                    <hr>";
+strVar += "                                    <div class=\"row reviewItemBody\">";
+strVar += "                                        <p class=\"reviewItemBodyDetails\">";
+strVar += "                                            <span class=\"reviewItemBodyRatedLabel\">Rated<\/span>";
+strVar += "                                            <span class=\"reviewItemBodyRating\">4.0<\/span>";
+strVar += "                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.";
+strVar += "                                        <\/p>";
+strVar += "                                    <\/div>";
+strVar += "                                <\/div>";
+strVar += "                                <div class=\"reviewItem\">";
+strVar += "                                    <div class=\" row reviewItemHeader\">";
+strVar += "                                        <div class=\"col-lg-1 col-md-1 col-sm-1 col-xs-1 zeroPadding\">";
+strVar += "                                            <a href=\"#\"><img src=\"img\/savatar1.png\" class=\"img-responsive\" style=\"width: 34px;height: 34px;\"><\/a>";
+strVar += "                                        <\/div>";
+strVar += "                                        <div class=\"col-lg-10 col-md-10 col-sm-10 col-xs-10\">";
+strVar += "                                            <div class=\"row\">";
+strVar += "                                                <a class=\"reviewItemHeaderName\" href=\"salon.html\">Salon 1<\/a>";
+strVar += "                                            <\/div>";
+strVar += "                                            <div class=\"row\">";
+strVar += "                                                <a class=\"reviewItemHeaderDetails\"> Elgin, Kolkata<\/a>";
+strVar += "                                            <\/div>";
+strVar += "                                        <\/div>";
+strVar += "                                        <div class=\"col-lg-1 col-md-1 col-sm-1 col-xs-1 zeroPadding\">";
+strVar += "                                            <span class=\"btn btn-default\"><i class=\"fa fa-heart-o\"><\/i><\/span>";
+strVar += "                                        <\/div>";
+strVar += "                                    <\/div>";
+strVar += "                                    <hr>";
+strVar += "                                    <div class=\"row reviewItemBody\">";
+strVar += "                                        <p class=\"reviewItemBodyDetails\">";
+strVar += "                                            <span class=\"reviewItemBodyRatedLabel\">Rated<\/span>";
+strVar += "                                            <span class=\"reviewItemBodyRating\">4.0<\/span>";
+strVar += "                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.";
+strVar += "                                        <\/p>";
+strVar += "                                    <\/div>";
+strVar += "                                <\/div>";
+strVar += "                                <div class=\"reviewItem\">";
+strVar += "                                    <div class=\" row reviewItemHeader\">";
+strVar += "                                        <div class=\"col-lg-1 col-md-1 col-sm-1 col-xs-1 zeroPadding\">";
+strVar += "                                            <a href=\"#\"><img src=\"img\/savatar1.png\" class=\"img-responsive\" style=\"width: 34px;height: 34px;\"><\/a>";
+strVar += "                                        <\/div>";
+strVar += "                                        <div class=\"col-lg-10 col-md-10 col-sm-10 col-xs-10\">";
+strVar += "                                            <div class=\"row\">";
+strVar += "                                                <a class=\"reviewItemHeaderName\" href=\"salon.html\">Salon 1<\/a>";
+strVar += "                                            <\/div>";
+strVar += "                                            <div class=\"row\">";
+strVar += "                                                <a class=\"reviewItemHeaderDetails\"> Elgin, Kolkata<\/a>";
+strVar += "                                            <\/div>";
+strVar += "                                        <\/div>";
+strVar += "                                        <div class=\"col-lg-1 col-md-1 col-sm-1 col-xs-1 zeroPadding\">";
+strVar += "                                            <span class=\"btn btn-default\"><i class=\"fa fa-heart-o\"><\/i><\/span>";
+strVar += "                                        <\/div>";
+strVar += "                                    <\/div>";
+strVar += "                                    <hr>";
+strVar += "                                    <div class=\"row reviewItemBody\">";
+strVar += "                                        <p class=\"reviewItemBodyDetails\">";
+strVar += "                                            <span class=\"reviewItemBodyRatedLabel\">Rated<\/span>";
+strVar += "                                            <span class=\"reviewItemBodyRating\">4.0<\/span>";
+strVar += "                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.";
+strVar += "                                        <\/p>";
+strVar += "                                    <\/div>";
+strVar += "                                <\/div>";
+strVar += "                                <div class=\"reviewItem\">";
+strVar += "                                    <div class=\" row reviewItemHeader\">";
+strVar += "                                        <div class=\"col-lg-1 col-md-1 col-sm-1 col-xs-1 zeroPadding\">";
+strVar += "                                            <a href=\"#\"><img src=\"img\/savatar1.png\" class=\"img-responsive\" style=\"width: 34px;height: 34px;\"><\/a>";
+strVar += "                                        <\/div>";
+strVar += "                                        <div class=\"col-lg-10 col-md-10 col-sm-10 col-xs-10\">";
+strVar += "                                            <div class=\"row\">";
+strVar += "                                                <a class=\"reviewItemHeaderName\" href=\"salon.html\">Salon 1<\/a>";
+strVar += "                                            <\/div>";
+strVar += "                                            <div class=\"row\">";
+strVar += "                                                <a class=\"reviewItemHeaderDetails\"> Elgin, Kolkata<\/a>";
+strVar += "                                            <\/div>";
+strVar += "                                        <\/div>";
+strVar += "                                        <div class=\"col-lg-1 col-md-1 col-sm-1 col-xs-1 zeroPadding\">";
+strVar += "                                            <span class=\"btn btn-default\"><i class=\"fa fa-heart-o\"><\/i><\/span>";
+strVar += "                                        <\/div>";
+strVar += "                                    <\/div>";
+strVar += "                                    <hr>";
+strVar += "                                    <div class=\"row reviewItemBody\">";
+strVar += "                                        <p class=\"reviewItemBodyDetails\">";
+strVar += "                                            <span class=\"reviewItemBodyRatedLabel\">Rated<\/span>";
+strVar += "                                            <span class=\"reviewItemBodyRating\">4.0<\/span>";
+strVar += "                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.";
+strVar += "                                        <\/p>";
+strVar += "                                    <\/div>";
+strVar += "                                <\/div>";
+strVar += "                                <div class=\"reviewItem\">";
+strVar += "                                    <div class=\" row reviewItemHeader\">";
+strVar += "                                        <div class=\"col-lg-1 col-md-1 col-sm-1 col-xs-1 zeroPadding\">";
+strVar += "                                            <a href=\"#\"><img src=\"img\/savatar1.png\" class=\"img-responsive\" style=\"width: 34px;height: 34px;\"><\/a>";
+strVar += "                                        <\/div>";
+strVar += "                                        <div class=\"col-lg-10 col-md-10 col-sm-10 col-xs-10\">";
+strVar += "                                            <div class=\"row\">";
+strVar += "                                                <a class=\"reviewItemHeaderName\" href=\"salon.html\">Salon 1<\/a>";
+strVar += "                                            <\/div>";
+strVar += "                                            <div class=\"row\">";
+strVar += "                                                <a class=\"reviewItemHeaderDetails\"> Elgin, Kolkata<\/a>";
+strVar += "                                            <\/div>";
+strVar += "                                        <\/div>";
+strVar += "                                        <div class=\"col-lg-1 col-md-1 col-sm-1 col-xs-1 zeroPadding\">";
+strVar += "                                            <span class=\"btn btn-default\"><i class=\"fa fa-heart-o\"><\/i><\/span>";
+strVar += "                                        <\/div>";
+strVar += "                                    <\/div>";
+strVar += "                                    <hr>";
+strVar += "                                    <div class=\"row reviewItemBody\">";
+strVar += "                                        <p class=\"reviewItemBodyDetails\">";
+strVar += "                                            <span class=\"reviewItemBodyRatedLabel\">Rated<\/span>";
+strVar += "                                            <span class=\"reviewItemBodyRating\">4.0<\/span>";
+strVar += "                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.";
+strVar += "                                        <\/p>";
+strVar += "                                    <\/div>";
+strVar += "                                <\/div>";
+strVar += "                                <div class=\"reviewItem\">";
+strVar += "                                    <div class=\" row reviewItemHeader\">";
+strVar += "                                        <div class=\"col-lg-1 col-md-1 col-sm-1 col-xs-1 zeroPadding\">";
+strVar += "                                            <a href=\"#\"><img src=\"img\/savatar1.png\" class=\"img-responsive\" style=\"width: 34px;height: 34px;\"><\/a>";
+strVar += "                                        <\/div>";
+strVar += "                                        <div class=\"col-lg-10 col-md-10 col-sm-10 col-xs-10\">";
+strVar += "                                            <div class=\"row\">";
+strVar += "                                                <a class=\"reviewItemHeaderName\" href=\"salon.html\">Salon 1<\/a>";
+strVar += "                                            <\/div>";
+strVar += "                                            <div class=\"row\">";
+strVar += "                                                <a class=\"reviewItemHeaderDetails\"> Elgin, Kolkata<\/a>";
+strVar += "                                            <\/div>";
+strVar += "                                        <\/div>";
+strVar += "                                        <div class=\"col-lg-1 col-md-1 col-sm-1 col-xs-1 zeroPadding\">";
+strVar += "                                            <span class=\"btn btn-default\"><i class=\"fa fa-heart-o\"><\/i><\/span>";
+strVar += "                                        <\/div>";
+strVar += "                                    <\/div>";
+strVar += "                                    <hr>";
+strVar += "                                    <div class=\"row reviewItemBody\">";
+strVar += "                                        <p class=\"reviewItemBodyDetails\">";
+strVar += "                                            <span class=\"reviewItemBodyRatedLabel\">Rated<\/span>";
+strVar += "                                            <span class=\"reviewItemBodyRating\">4.0<\/span>";
+strVar += "                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.";
+strVar += "                                        <\/p>";
+strVar += "                                    <\/div>";
+strVar += "                                <\/div>";
+
+
+
+$(document).ready(function () {
+    $('#page-selection').bootpag({
+        total: 10,
+        page: 1,
+        maxVisible: 5,
+        leaps: true,
+        firstLastUse: true,
+        first: '←',
+        last: '→',
+        wrapClass: 'pagination',
+        activeClass: 'active',
+        disabledClass: 'disabled',
+        nextClass: 'next',
+        prevClass: 'prev',
+        lastClass: 'last',
+        firstClass: 'first'
+    }).on("page", function (event, num) {
+        $("#searchResultListID").html(strVar);
+
+    });
+});
+
